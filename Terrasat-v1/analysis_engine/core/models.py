549a2aece01286d42152dcaa2e05406634c0aafc
mwigoti <henrymwoha02@gmail.com>
2025-08-06 09:52:23 +0300

    models

--- Terrasat-v1/analysis_engine/core/models.py
+++ Terrasat-v1/analysis_engine/core/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from django.utils.timezone import now
+from django.contrib.gis.db import models
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+    
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+class CustomUser(AbstractUser):
+    
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    location = models.CharField(max_length=100, blank=True, null=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    registered_via_ussd = models.BooleanField(default=False)
+    ussd_registration_date = models.DateTimeField(null=True, blank=True)
+
+    
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+class Field(models.Model):
+    owner=models.ForeignKey(CustomUser, on_delete=models.CASCADE, default=True)
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    geometry = models.PolygonField(srid=3857, geography=False)  
+    created_at = models.DateTimeField(default=now)
+    area = models.FloatField(default=0.0)
+
+    def __str__(self):
+        return self.name
+    
