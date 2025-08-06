f7ae298d7d55595379337da255c0e1fa27f655d1
mwigoti <henrymwoha02@gmail.com>
2025-08-06 09:55:45 +0300

    register_models

--- Terrasat-v1/analysis_engine/core/admin.py
+++ Terrasat-v1/analysis_engine/core/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib.admin.sites import AlreadyRegistered
+from django.apps import apps
+
+
+models = apps.get_models()
+
+
+for model in models:
+    try:
+        admin.site.register(model)
+    except AlreadyRegistered:
+        pass
+
