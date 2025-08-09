883a8e6b969dc09923860f7cd81209947088e1ff
JoyyCLangat <121197381+JoyyCLangat@users.noreply.github.com>
2025-08-09 15:03:14 +0300

    Connecting to Polygon

---
+++ Terrasat-v1/Blockchain/terrasat-smart-contracts/test/CarbonCreditToken.t.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import "forge-std/Test.sol";
+import "../src/CarbonCreditToken.sol";
+
+contract CarbonCreditTokenTest is Test {
+    CarbonCreditToken token;
+
+    function setUp() public {
+        token = new CarbonCreditToken();
+    }
+
+    function testMint() public {
+        token.mint("proj-1", 2025, "Reforestation", "0xabc123", 100);
+        CarbonCreditToken.Credit memory credit = token.getCredit(0);
+        assertEq(credit.vintage, 2025);
+    }
+}
